0e6d5acf591e52ee8249cf152f8a40d3fc3179cc
Paul J. Shaw <pavlvsxavier@gmail.com>
2021-03-10 11:17:19 +0000

    detail page

---
+++ detail.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'app/app.php';
+
+if (!isset($_GET['term'])) {
+    redirect('index.php');
+}
+
+$data = getTerm($_GET['term']); // TODO: validate input
+
+if ($data == false) {
+    view('not_found');
+    die();
+}
+
+$viewBag = [
+    'title' => 'Detail for ' . $data->term
+];
+
+view('detail', $data);
